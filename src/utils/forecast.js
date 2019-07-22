92bb4d42cadcb85b25e78e276195b7c333c6f5fd
SOIN <soin@SOINs-MacBook-Pro.local>
2019-07-22 14:25:03 -0600

    Cambio en el idioma

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/bd935a637cf30df82ae830152c91a02b/'+latitude+','+longitude+'?lang=es'
-    console.log(url)
+    
     request({url, json: true}, (error, {body}) => {
         if(error){
            callback('No se puede acceder al servicio climato', undefined)
